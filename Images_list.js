ae85ee98937858a74bf2e1afcc48ba8c23e0ce94
Vijay Babu <mrvijaybabu77@gmail.com>
2025-08-14 01:48:08 +0530

    Images_list.js

--- Images_list.js
+++ Images_list.js
@@ -4,7 +4,15 @@ const wallpaperTitles = {
   'krishna-cow.jpg': 
   'krishna-leela.jpg': 
   'radha-krishna.jpg': 
-  'krishna-sudama.jpg':  
+  'krishna-sudama.jpg': 
+  'pexels-media.jpg':
+  'pexels-media01.jpg':
+  'pexels-media02.jpg':
+  'pexels-media03.jpg':
+  'pexels-media04.jpg':
+  'pexels-media05.jpg':
+  'pexels-media06.jpg':
+'radha-krishna.jpg': 
 };
 
 export default wallpaperTitles;
