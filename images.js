0a5347817fa3f25bdf8810b5a7597dfec2230125
Vijay Babu <mrvijaybabu77@gmail.com>
2025-08-14 07:31:31 +0530

    images.js

--- images.js
+++ images.js
@@ -1,18 +1,18 @@
-const wallpaperTitles = {
-  'krishna.png':
-  'krishna-flute.jpg': 
-  'krishna-cow.jpg': 
-  'krishna-leela.jpg': 
-  'radha-krishna.jpg': 
-  'krishna-sudama.jpg': 
-  'pexels-media.jpg':
-  'pexels-media01.jpg':
-  'pexels-media02.jpg':
-  'pexels-media03.jpg':
-  'pexels-media04.jpg':
-  'pexels-media05.jpg':
-  'pexels-media06.jpg':
-'radha-krishna.jpg': 
-};
+var wallpaperTitles = [
+  'krishna.png',
+  'krishna-flute.jpg',
+  'krishna-cow.jpg',
+  'krishna-leela.jpg',
+  'radha-krishna.jpg',
+  'krishna-sudama.jpg',
+  'pexels-media.jpg',
+  'pexels-media01.jpg',
+  'pexels-media02.jpg',
+  'pexels-media03.jpg',
+  'pexels-media04.jpg',
+  'pexels-media05.jpg',
+  'pexels-media06.jpg',
+'radha-krishna.jpg'
+];
 
 export default wallpaperTitles;
