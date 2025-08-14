1a4d9ec96e8ee996e29f205f25d8350b7cddad35
Vijay Babu <mrvijaybabu77@gmail.com>
2025-08-14 07:41:08 +0530

    images.js

--- images.js
+++ images.js
@@ -1,16 +1,18 @@
+Javascript 
+
 var images = [
-  'krishna.png',
-  'krishna-flute.jpg',
-  'krishna-cow.jpg',
-  'krishna-leela.jpg',
-  'radha-krishna.jpg',
-  'krishna-sudama.jpg',
-  'pexels-media.jpg',
-  'pexels-media01.jpg',
-  'pexels-media02.jpg',
-  'pexels-media03.jpg',
-  'pexels-media04.jpg',
-  'pexels-media05.jpg',
-  'pexels-media06.jpg',
-'radha-krishna.jpg'
+  "krishna.png",
+  "krishna-flute.jpg",
+  "krishna-cow.jpg",
+  "krishna-leela.jpg",
+  "radha-krishna.jpg",
+  "krishna-sudama.jpg",
+  "pexels-media.jpg",
+  "pexels-media01.jpg",
+  "pexels-media02.jpg",
+  "pexels-media03.jpg",
+  "pexels-media04.jpg",
+  "pexels-media05.jpg",
+  "pexels-media06.jpg",
+  "radha-krishna.jpg"
 ];
